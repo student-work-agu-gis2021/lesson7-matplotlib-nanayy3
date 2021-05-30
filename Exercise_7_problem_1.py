a92c7d20e1be16fa817a153ae2f255b032370259
nanayy3 <>
2021-05-30 23:00:54 +0000

    I read the data into data and parse dates.

--- Exercise_7_problem_1.py
+++ Exercise_7_problem_1.py
@@ -75,6 +75,7 @@ plt.show()
 outputfp = "my_first_plot.png"
 
 # YOUR CODE HERE 5
+plt.savefig(outputfp)
 
 # This test print statement should print the output filename of your figure
 print("Saved my first plot as:", outputfp)
